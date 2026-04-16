cdb2252758e74d0d763d973fa4bb17523d5f4808
blackbox24 <nicardid@gmail.com>
2026-04-16 19:10:13 +0000

    chore: wrote test to test creation of users

--- users/tests.py
+++ users/tests.py
@@ -7,7 +7,19 @@ from users.models import User
 
 class CustomUserTest(TestCase):
     def test_create_user_successfull(self):
-        User.objects.create(username="test", password="password")
+        User.objects.create(username="test", password="password", role="user")
         is_ava = User.objects.filter(username="test").exists()
 
         self.assertTrue(is_ava, True)
+
+    def test_create_default_role_successfull(self):
+        User.objects.create(username="test", password="password")
+        user = User.objects.get(username="test")
+
+        self.assertTrue(user.role, "USER")
+
+    def test_create_admin_role_successfull(self):
+        User.objects.create(username="test", password="password", role="admin")
+        user = User.objects.get(username="test")
+
+        self.assertTrue(user.role, "ADMIN")
