9cbd48f0c41a50e492ebde2fdcd27720b4a704f3
blackbox24 <nicardid@gmail.com>
2026-04-16 19:41:42 +0000

    feat: login api

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,11 +1,14 @@
 from django.urls import reverse
-from rest_framework.status import HTTP_201_CREATED
+from rest_framework.status import HTTP_201_CREATED, HTTP_200_OK
 from rest_framework.test import APITestCase
 
 from users.models import User
 
 
 class AuthTestCase(APITestCase):
+    def setUp(self) -> None:
+        self.test_user = User.objects.create_user(username="test_user",password="test_password")
+
     def test_signup_successful(self, *args, **kwargs):
         url = reverse("signup_view")
         response = self.client.post(url, data={
@@ -17,3 +20,10 @@ class AuthTestCase(APITestCase):
         is_ava = User.objects.filter(username="test_username").exists()
         self.assertEqual(response.status_code, HTTP_201_CREATED)
         self.assertTrue(is_ava)
+
+    def test_login_successful(self):
+        url = reverse("login_view")
+        response = self.client.post(url, data={"username": self.test_user.username, "password": "test_password"})
+        self.assertEqual(response.status_code, HTTP_200_OK)
+
+from django.core.management.utils import get_random_secret_key
