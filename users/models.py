2acfe9f40098640690cdf1a749683106fc32bdf4
blackbox24 <nicardid@gmail.com>
2026-04-16 19:07:15 +0000

    chore: created models for user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    ROLES = (("user", "USER"), ("admin", "ADMIN"))
+    role = models.CharField(max_length=6, choices=ROLES, default="user")
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
+from users.models import User
+
 # Create your tests here.
+
+
+class CustomUserTest(TestCase):
+    def test_create_user_successfull(self):
+        User.objects.create(username="test", password="password")
+        is_ava = User.objects.filter(username="test").exists()
+
+        self.assertTrue(is_ava, True)
