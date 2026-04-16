6571f96a5862da9048229c48e071ce727af14d04
blackbox24 <nicardid@gmail.com>
2026-04-16 19:33:51 +0000

    chore: added login and refresh token url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import views
 
 urlpatterns = [
     path("signup/", views.SignUpView.as_view(), name="signup_view"),
+    path("login/", TokenObtainPairView.as_view(), name="login_view"),
+    path("refresh/", TokenRefreshView.as_view(), name="refresh_token_view")
 ]
